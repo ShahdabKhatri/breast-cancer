be1283c688fcdafa29f5ef31f3bfc48a249fab80
Shahdab <ShahdabKhatri@gmail.com>
2020-10-02 22:55:34 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# breas cancer web app deployed on Heroku
+
+The deployed web app is live at https://breast-cancer-by-shahdab.herokuapp.com/
+
+This web app predicts the type of breast cancer as a function of their input parameters ( Mean Perimeter, Mean_Area, Area_Error, Worst_Perimeter,Worst Area ).
+
+The web app was built in Python using the following libraries:
+* streamlit
+* pandas
+* pickle
+
+
---
+++ penguins-app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+st.write("""
+# Simple Breast Cancer Detection App
+
+This app predicts the **Breast Cancer** type!
+""")
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    Mean_Perimeter = st.sidebar.slider('Mean Perimeter', 35.0,180.0,122.8)
+    Mean_Area = st.sidebar.slider('Mean Area', 120.0, 1900.0,1001.0)
+    Area_Error = st.sidebar.slider('Area Error', 7.0, 200.0,153.4)
+    Worst_Perimeter = st.sidebar.slider('Worst Perimeter', 40.0, 220.0,184.6)
+    Worst_Area = st.sidebar.slider('Worst Area', 170.0,3500.0,2019.0)
+    data = {'Mean Perimeter': Mean_Perimeter,
+            'Mean Area': Mean_Area,
+            'Area Error': Area_Error,
+            'Worst Perimeter': Worst_Perimeter,
+            'Worst Area':Worst_Area}
+    
+    features = pd.DataFrame(data, index=[0])
+    return features
+
+df = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(df)
+
+clf = pickle.load(open('penguins_clf', 'rb'))
+
+prediction = clf.predict(df)
+prediction_proba = clf.predict_proba(df)
+
+#st.subheader('Class labels and their corresponding index number')
+#st.write(iris.target_names)
+mapping={0:'malignant',1:'benign'}
+st.subheader('Prediction')
+st.write(mapping[prediction[0]])
+
+
+
+st.subheader('Prediction Probability')
+st.write(prediction_proba)
